3fd9eb8a4c82c2d7ae1a21db413070217341b0a1
Keith Glasnapp <kglasnapp@gmail.com>
2024-01-15 17:53:22 -0500

    Shooter test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,155 +1,55 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import java.util.Optional;
-
-import edu.wpi.first.wpilibj.DriverStation;
+// import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
-
-/* 
-Optional<Alliance> ally = DriverStation.getAlliance();
-if (ally.isPresent()) {
-    if (ally.get() == Alliance.Red) {
-        <RED ACTION>
-    }
-    if (ally.Get() == Alliance.Blue) {
-        <BLUE ACTION>
-    }
-}
-else {
-    <NO COLOR YET ACTION>
-}
-
-*/
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command autonomousCommand;
+  CANSparkBase motorR = new CANSparkFlex(1, MotorType.kBrushless);
+  CANSparkBase motorL = new CANSparkFlex(2, MotorType.kBrushless);
+  //CANcoder canCoder = new CANcoder(9, "RIO");
 
-  public RobotContainer robotContainer;
-  public static int count = 0;
-  public static Optional<Alliance> alliance;
-  // public static Alliance alliance;
+  XboxController cont = new XboxController(2);
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any
-   * initialization code.
-   */
+  boolean voltageControl = false;
+
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-    // and put our
-    // autonomous chooser on the dashboard.
-    robotContainer = new RobotContainer();
-    alliance = DriverStation.getAlliance();
-
-    Util.logf("Start Sibling Alliance:%s\n", alliance.toString());
-    RobotContainer.leds.setAllianceLeds();
+    motorL.getPIDController().setP(0.1);
+    // motorL.getPIDController().setD(0.2);
+    motorR.getPIDController().setP(0.1);
+    // motorR.getPIDController().setD(0.2);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for
-   * items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and
-   * test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    count++;
-    CommandScheduler.getInstance().run();
-    if (count % 25 == 9) {
-      SmartDashboard.putNumber("Memory", getMemory());
-    }
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /**
-   * Autonomous runs the autonomous command selected by your
-   * {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = Autonomous.autonomousChooser.getSelected();
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
-    alliance = DriverStation.getAlliance();
-    Util.logf("Enable Robot Alliance: %s\n", alliance.toString());
-    RobotContainer.leds.setAllianceLeds();
-  }
-
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-  }
+    int pov = cont.getPOV();
+    double value = 0.0;
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    if (pov == 0) {
+      value = 0.1;
+      // value = 2000;
+    } else if (pov == 90) {
+      value = 0.25;
+      // value = 3000;
+    } else if (pov == 180) {
+      value = 0.5;
+      // value = 4000;
+    } else if (pov == 270) {
+      value = 0.7;
+      // value = 5000;
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
+    motorL.set(value);
+    motorR.set(value);
 
-  public long getMemory() {
-    //return Runtime.getRuntime().freeMemory();
-    return Runtime.getRuntime().freeMemory();
+    // motorL.getPIDController().setReference(value,
+    // CANSparkBase.ControlType.kVelocity);
+    // motorR.getPIDController().setReference(value,
+    // CANSparkBase.ControlType.kVelocity);
   }
 }
