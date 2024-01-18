f30e5b876630be1c44dda7ed41cb452cc20034a2
Keith Glasnapp <kglasnapp@gmail.com>
2024-01-17 20:50:28 -0500

    More testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 // import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkBase;
@@ -16,9 +18,10 @@ import edu.wpi.first.wpilibj.XboxController;
  * 8ft: 42deg 0.7 power
 */
 public class Robot extends TimedRobot {
-  CANSparkBase motorR = new CANSparkFlex(1, MotorType.kBrushless);
-  CANSparkBase motorL = new CANSparkFlex(2, MotorType.kBrushless);
+  CANSparkBase motor1 = new CANSparkFlex(1, MotorType.kBrushless);
+  CANSparkBase motor2 = new CANSparkFlex(2, MotorType.kBrushless);
   //CANcoder canCoder = new CANcoder(9, "RIO");
+  //TalonSRX intake = new TalonSRX(14);
 
   XboxController cont = new XboxController(2);
 
@@ -26,10 +29,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    motorL.getPIDController().setP(0.1);
-    // motorL.getPIDController().setD(0.2);
-    motorR.getPIDController().setP(0.1);
-    // motorR.getPIDController().setD(0.2);
+    motor1.getPIDController().setP(3e-4);
+    motor1.getPIDController().setI(4e-7);
+    motor1.getPIDController().setD(1e-3);
+    motor2.getPIDController().setP(3e-4);
+    motor2.getPIDController().setI(4e-7);
+    motor2.getPIDController().setD(1e-3);
   }
 
   @Override
@@ -37,22 +42,25 @@ public class Robot extends TimedRobot {
     int pov = cont.getPOV();
     double value = 0.0;
 
-    if (pov == 0) {
-      value = 0.1;
-      // value = 2000;
-    } else if (pov == 90) {
-      value = 0.25;
-      // value = 3000;
-    } else if (pov == 180) {
+    if (cont.getYButton()) {
       value = 0.5;
-      // value = 4000;
-    } else if (pov == 270) {
+      // value = 2000;
+    } else if (cont.getBButton()) {
       value = 0.7;
+      // value = 3000;
+    } else if (cont.getAButton()) {
+      value = 0.85;
+      // value = 4000;
+    } else if (cont.getXButton()) {
+      value = 1.0;
       // value = 5000;
     }
 
-    motorL.set(value);
-    motorR.set(value);
+    motor1.set(value*.9);
+    motor2.set(value);
+
+    double speed = cont.getLeftY();
+    //intake.set(TalonSRXControlMode.PercentOutput, speed);
 
     // motorL.getPIDController().setReference(value,
     // CANSparkBase.ControlType.kVelocity);
