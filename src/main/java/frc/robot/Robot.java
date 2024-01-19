5c44ab1abd0db66f12085fe1e1c24c6ea1b36237
Keith Glasnapp <kglasnapp@gmail.com>
2024-01-18 20:35:58 -0500

    climber motor position pid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,10 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.PositionVoltage;
 // import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -16,16 +17,18 @@ import edu.wpi.first.wpilibj.XboxController;
  * 6ft: 50deg 0.7 power
  * 7ft: 44deg 0.7 power
  * 8ft: 42deg 0.7 power
-*/
+ */
 public class Robot extends TimedRobot {
-  CANSparkBase motor1 = new CANSparkFlex(1, MotorType.kBrushless);
-  CANSparkBase motor2 = new CANSparkFlex(2, MotorType.kBrushless);
-  //CANcoder canCoder = new CANcoder(9, "RIO");
-  //TalonSRX intake = new TalonSRX(14);
+  final CANSparkBase motor1 = new CANSparkFlex(1, MotorType.kBrushless);
+  final CANSparkBase motor2 = new CANSparkFlex(2, MotorType.kBrushless);
+  // CANcoder canCoder = new CANcoder(9, "RIO");
+  // TalonSRX intake = new TalonSRX(14);
+  final TalonFX climber = new TalonFX(10);
+  final PositionVoltage request = new PositionVoltage(0).withSlot(0);
 
-  XboxController cont = new XboxController(2);
+  final XboxController cont = new XboxController(2);
 
-  boolean voltageControl = false;
+  final boolean voltageControl = false;
 
   @Override
   public void robotInit() {
@@ -35,6 +38,20 @@ public class Robot extends TimedRobot {
     motor2.getPIDController().setP(3e-4);
     motor2.getPIDController().setI(4e-7);
     motor2.getPIDController().setD(1e-3);
+
+    // Configure the TalonFX for basic use
+    Slot0Configs configs = new Slot0Configs();
+
+    // This TalonFX should be configured with a kP of 1, a kI of 0, a kD of 10, and
+    // a kV of 2 on slot 0
+    configs.kP = 1;
+    configs.kI = 0;
+    configs.kD = 0;
+    //configs.Slot0.kV = 2;
+
+    // Write these configs to the TalonFX
+    climber.getConfigurator().apply(configs);
+    climber.setSafetyEnabled(true);
   }
 
   @Override
@@ -56,15 +73,17 @@ public class Robot extends TimedRobot {
       // value = 5000;
     }
 
-    motor1.set(value*.9);
+    motor1.set(value * .9);
     motor2.set(value);
 
     double speed = cont.getLeftY();
-    //intake.set(TalonSRXControlMode.PercentOutput, speed);
+    // intake.set(TalonSRXControlMode.PercentOutput, speed);
+    // climber.set(speed);
+    climber.setControl(request.withPosition(speed * 200));
 
-    // motorL.getPIDController().setReference(value,
+    // motor1.getPIDController().setReference(value,
     // CANSparkBase.ControlType.kVelocity);
-    // motorR.getPIDController().setReference(value,
+    // motor2.getPIDController().setReference(value,
     // CANSparkBase.ControlType.kVelocity);
   }
 }
